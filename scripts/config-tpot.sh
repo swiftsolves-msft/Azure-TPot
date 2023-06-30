6caebd2cc812d064da7aa886607c302ae0d25e47
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2023-06-29 20:09:46 -0400

    Update config-tpot.sh

--- scripts/config-tpot.sh
+++ scripts/config-tpot.sh
@@ -10,11 +10,12 @@ sudo git clone https://github.com/telekom-security/tpotce /root/tpot
 # download config file
 sudo wget https://raw.githubusercontent.com/swiftsolves-msft/Azure-TPot/main/scripts/tpot.conf -O /root/tpot.conf
 sudo chmod 777 /root/tpot.conf
-sudo mkdir -p /opt/tpot/etc
-sudo wget https://github.com/telekom-security/tpotce/blob/master/etc/compose/standard.yml -O /opt/tpot/etc/tpot.yml
 
 # install Tpot
 sudo /root/tpot/iso/installer/install.sh --type=auto --conf=/root/tpot.conf
+
+# symlink
+sudo wget https://github.com/telekom-security/tpotce/blob/master/etc/compose/standard.yml -O /opt/tpot/etc/tpot.yml
 
 # remove tpot.conf
 rm /root/tpot.conf
